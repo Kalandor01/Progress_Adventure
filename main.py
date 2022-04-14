ac9c3a1adff26ae2e42447c3534347195ff69a8e
Kalandor01 <54396021+Kalandor01@users.noreply.github.com>
2022-04-15 01:14:59 +0200

    seed save + deleted deprecated + menu_ui

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import random_sentance as rs
 import save_file_manager as sfm
 
 import classes as cl
+from classes import r
 
 
 def imput(ask="Num: ", type=int):
@@ -19,21 +20,9 @@ def imput(ask="Num: ", type=int):
         try: return type(input(ask))
         except ValueError: print(f'Not{" whole" if type == int else ""} number!')
 
-# variables
-game = True
-# SEED
-r_seed = np.random.RandomState()
-seed = r_seed.randint(0, 1000000000)
-r = np.random.RandomState(seed)
 
+# dummy player for global
 player = cl.Player()
-# name = "You"
-# life = r.randint(1, 6) + r.randint(1, 6) + 12
-# skill = r.randint(1, 6) + 6
-# luck = round(r.random() * 100 / 5, 1)
-gold = 0
-no = False
-page = 1
 
 
 # Monster cheat sheet: name, life, attack, deff, speed, rare, team, switched
@@ -183,110 +172,7 @@ def stats(won=0):
         print(f"HP: {player.hp}")
     print(f"Attack: {player.attack}\nDefence: {player.defence}\nSpeed: {player.speed}\n")
     if won == 0 or won == 1:
-        print(f"OTHER:\nGold: {gold}")
-
-
-# + extra: full heal...                     DEPRECATED?
-def modify(stat, value=0, write=True):
-    global life, gold
-    match stat:
-        case "life":
-            life += value
-            if write:
-                print(f"HP: {life}")
-        case "skill":
-            skill += value
-            if write:
-                print(f"Skill: {skill}")
-        case "luck":
-            luck += value
-            if write:
-                print(f"Luck: {luck}")
-        case "gold":
-            gold += value
-            if write:
-                print(f"Gold: {gold}")
-        case _:
-            logging.warning(f"{stat} is not a valid stat!!!")
-
-
-# pages                                     DEPRECATED
-def pages(p):
-    global page, life, gold, no
-    page = p
-    match page:
-        case 2:
-            print("\nLátod, hogy az akadály egy széles, barna, sziklaszerű tárgy. Megérinted, és meglepve tapasztalod, hogy lágy, szivacsszerű.\n1: Ha át szeretnél mászni rajta.\n2: Ha ketté akarod vágni a kardoddal.")
-            page = 2
-        case 3:
-            print("\nNéhány perc gyaloglás után egy elágazáshoz érsz az alagútban. Egy, a falra festett fehér nyíl nyugatfelé mutat. A földön nedves lábnyomok jelzik, merre haladtak az előtted járók. Nehéz biztosan megmondani, de úgy tűnik, hogy három közülük a nyíl irányába halad, míg egyikük úgy döntött, hogy klifenek megy.\n1: Ha nyugat felé kívánsz menni.\n2: Ha keletnek.")
-            page = 3
-        case 4:
-            print("\nAhogy végigmész az alagúton, csodálkozva látod, hogy egy jókora vasharang csüng alá a boltozatról.")
-            stats()
-        case 5:
-            print("\nKardod könnyedén áthatol a spóragolyó vékonykülső burkán. Sűrű barna spórafelhő csap ki a golyóból, és körülvesz. Némelyik spóra a bőrödhöz tapad, és rettenetes viszketést okoz. Nagy daganatok nőnek az arcodon és karodon, és a bőröd mintha égne. 2 ÉLETERŐ pontot veszítesz. Vadul vakarózva átléped a leeresztett golyót, és klifenek veszed az utad.")
-            modify("life", -2)
-            stats()
-        case 6:
-            print("\nA doboz teteje könnyedén nyílik. Benne két aranypénzt találsz, és egy üzenetet, amely egy kis pergamenen neked szól. Előbb zsebre vágod az aranyakat, aztán elolvasod az üzenetet: - „Jól tetted. Legalább volt annyi eszed, hogy megállj és elfogadd az ajándékot. Most azt tanácsolom neked, hogy keress és használj különféle tárgyakat, ha sikerrel akarsz áthaladni Halállabirintusomon.” Az aláírás Szukumvit. Megjegyzed a tanácsot, apródarabokra téped a pergament, és tovább mész észak felé.")
-            modify("gold", 2)
-            page = 6
-            pages(3)
-        case 7:
-            print("\nA három pár nedves lábnyomot követve az alagút nyugati elágazásában hamarosan egy újabb elágazáshoz érsz.\n1: Ha továbbmész nyugat felé a lábnyomokat követve.\n2: Ha inkább észak felé mész a harmadik pár lábnyom után.")
-            page = 7
-        case 8:
-            print("\nFölmászol a lágy sziklára, attól tartasz, hogy bár-melyik pillanatban elnyelhet. Nehéz átvergődni rajta, mert puha anyagában alig tudod a lábadat emelni, de végül átvergődsz rajta. Megkönnyebbülten érsz újra szilárd talajra, és fordulsz klife felé.")
-            stats()
-        case 9:
-            print("\nHallod, hogy elölről súlyos lépések közelednek. Egy széles, állatbőrökbe öltözött, kőbaltás, primitívlény lép elő. Ahogy meglát, morog, a földre köp, majd a kőbaltát felemelve közeledik, és mindennek kinéz, csak barátságosnak nem. Előhúzod kardodat, és felkészülsz, hogy megküzdj a Barlangi Emberrel.")
-            fight([cl.Caveman()])
-            stats(1)
-        case _:
-            no = True
-            logging.warning("Wrong page number!!!")
-
-
-# page choice                                   DEPRECATED
-def page_turning():
-    global game, seed, name, life, gold, no, page
-    page_old = page
-    try:
-        page = int(imput("\nWhich page do you want to go to?: "))
-    except ValueError:
-        page = -1
-    if page == 1:
-        match page_old:
-            case 1:
-                pages(6)
-            case 2:
-                pages(8)
-            case 3:
-                pages(7)
-            case 7:
-                pages(4)
-            case _:
-                no = True
-    elif page == 2:
-        match page_old:
-            case 1:
-                pages(3)
-            case 2:
-                pages(5)
-            case 3:
-                pages(2)
-            case 7:
-                pages(9)
-            case _:
-                no = True
-    else:
-        no = True
-    # wrong
-    if no:
-        print("Wrong page number!")
-        page = page_old
-        no = False
+        print(f"OTHER:")
     
 
 def game_loop(data:list=None):
@@ -296,17 +182,20 @@ def game_loop(data:list=None):
 
     
 def new_save(save_num=1, save_name="save*"):
-    new_save_data = []
-    # get new save data
-    global game, name
+    # make player
+    # cl.r.set_state(r.get_state())
+    print(r.get_state())
     name = input("What is your name?: ")
     if name == "":
         name = "You"
     player.name = name
     stats(-1)
+    # get new save data
+    new_save_data = []
     today = datetime.today()
     new_save_data.append(f"{today.year}, {today.month}, {today.day}, {today.hour}, {today.minute}, {today.second}")
     new_save_data.append(f"{player.name}, {player.hp}, {player.attack}, {player.defence}, {player.speed}")
+    new_save_data.append(random_state_converter(r))
     # write new save
     sfm.encode_save(new_save_data, save_num, save_name)
     data = []
@@ -327,9 +216,119 @@ def load_save(save_num=1, save_name="save*"):
     player.defence = int(player_data[3])
     player.speed = float(player_data[4])
     stats(-1)
+    r.set_state(random_state_converter(datas[2]))
+    print(r.get_state())
     data = []
     game_loop(data)
 
+
+def manage_saves(file_data, max_saves=5, save_name="save*", save_ext="sav", can_exit=False):
+    from os import remove
+
+    in_main_menu = True
+    while True:
+        if len(file_data):
+            if in_main_menu:
+                in_main_menu = False
+                option = sfm.UI_list(["New save", "Load/Delete save"], " Main menu", can_esc=can_exit).display()
+            else:
+                option = 1
+            # new file
+            if option == 0:
+                new_slot = 1
+                for data in file_data:
+                    if data[0] == new_slot:
+                        new_slot += 1
+                return [1, new_slot]
+            elif option == -1:
+                return [-1, -1]
+            # load/delete
+            else:
+                # get data from file_data
+                list_data = []
+                for data in file_data:
+                    list_data.append(data[1])
+                    list_data.append(None)
+                list_data.append("Delete file")
+                list_data.append("Back")
+                option = sfm.UI_list(list_data, " Level select", multiline=True, can_esc=True).display()
+                # load
+                if option != -1 and option / 2 < len(file_data):
+                    return [0, file_data[int(option / 2)][0]]
+                # delete
+                elif option / 2 == len(file_data):
+                    list_data.pop(len(list_data) - 2)
+                    delete_mode = True
+                    while delete_mode and len(file_data) > 0:
+                        option = sfm.UI_list(list_data, " Delete mode!", "X ", "  ", multiline=True, can_esc=True).display()
+                        if option != -1 and option != len(list_data) - 1:
+                            option = int(option / 2)
+                            if sfm.UI_list(["No", "Yes"], f" Are you sure you want to remove Save file {file_data[option][0]}?", can_esc=True).display():
+                                remove(f'{save_name.replace("*", str(file_data[option][0]))}.{save_ext}')
+                                if option == len(file_data) - 1:
+                                    max_saves -= 1
+                                    metadata_manager(0, max_saves)
+                                list_data.pop(option * 2)
+                                list_data.pop(option * 2)
+                                file_data.pop(option)
+                        else:
+                            delete_mode = False
+                # back
+                else:
+                    in_main_menu = True
+        else:
+            input(f"\n No save files detected!")
+            return [1, 1]
+
+
+def metadata_manager(line_num:int, write_value=None):
+    """
+    STRUCTURE:\n
+    0 = max_saves
+    """
+    # default values
+    max_saves = 5
+    try:
+        metadata = sfm.decode_save(0, "metadata")
+    except FileNotFoundError:
+        metadata = [max_saves]
+        sfm.encode_save(metadata, 0, "metadata")
+    # formating
+    metadata[0] = int(metadata[0])
+    if write_value == None:
+        return metadata[line_num]
+    else:
+        metadata[line_num] = write_value
+        sfm.encode_save(metadata, 0, "metadata")
+
+
+def random_state_converter(random_state:np.random.RandomState | tuple | str):
+    """
+    Can convert a numpy RandomState.getstate() into an easily storable string and back.
+    """
+    if type(random_state) == str:
+        lines = random_state.split(";")
+        states = [int(num) for num in lines[1].split("|")]
+        return (str(lines[0]), np.array(states, dtype=np.uint32), int(lines[2]), int(lines[3]), float(lines[4]))
+    else:
+        if type(random_state) == tuple:
+            state = random_state
+        else:
+            state = random_state.get_state()
+        state_txt = state[0] + ";"
+        for num in state[1]:
+            state_txt += str(num) + "|"
+        return f"{state_txt[:-1]};{str(state[2])};{str(state[3])};{str(state[4])}"
+
+
+def decode_save_file(save_num=1, save_name="save*", save_ext="sav"):
+    save_data = sfm.decode_save(save_num, save_name, save_ext)
+    f = open(f'{save_name.replace("*", str(save_num))}.decoded.txt', "w")
+    for line in save_data:
+        f.write(line + "\n")
+    f.close()
+
+decode_save_file()
 
 def main():
     """
@@ -345,35 +344,40 @@ def main():
     """
 
     save_name = os.path.dirname(os.path.abspath(__file__)) + "/save*"
-    max_saves = 5
 
-    options_status = True
-    while options_status:
-        # get save datas
-        datas = sfm.file_reader(max_saves, save_name=save_name)
-        # write id data
-        for data in datas:
-            print(f"Save file {data[0]}: {data[1][1].split(', ')[0]}")
-            last_acces = data[1][0].split(", ")
-            print(f"Last opened: {last_acces[0]}.{'0' if int(last_acces[1]) < 10 else ''}{last_acces[1]}.{last_acces[2]} {last_acces[3]}:{last_acces[4]}:{last_acces[5]}\n")
-        # manage saves
-        status = sfm.manage_saves(datas, max_saves, save_name)
-        if status[0] == 1 or status[0] == 0:
-            options_status = False
-        else:
-            print(f"Deleted save in slot {status[1]}!")
+    # get max saves
+    max_saves = metadata_manager(0)
+
+    # get save datas
+    datas = sfm.file_reader(max_saves, save_name=save_name)
+    # write id data
+    datas_processed = []
+    for data in datas:
+        data_processed = ""
+        data_processed += f"Save file {data[0]}: {data[1][1].split(', ')[0]}\n"
+        last_acces = data[1][0].split(", ")
+        data_processed += f"Last opened: {last_acces[0]}.{'0' if int(last_acces[1]) < 10 else ''}{last_acces[1]}.{last_acces[2]} {last_acces[3]}:{last_acces[4]}:{last_acces[5]}"
+        datas_processed.append([data[0], data_processed])
+    # manage saves
+    status = manage_saves(datas_processed, max_saves, save_name, "sav", True)
+    # new save
     if status[0] == 1:
-        input(f"New game in slot {status[1]}!")
+        input(f"\nNew game in slot {status[1]}!")
+        # new slot?
+        if status[1] > max_saves:
+            max_saves += 1
+            metadata_manager(0, max_saves)
         new_save(status[1], save_name)
+    # load
     elif status[0] == 0:
-        input(f"Loading slot {status[1]}!")
+        input(f"\nLoading slot {status[1]}!")
         load_save(status[1], save_name)
 
 # print(cl.Player())
 
 if __name__ == "__main__":
     error_handling = False
-    # ultimate error handlind (release only)
+    # ultimate error handelind (release only)
     if error_handling:
         exit_game = False
         while not exit_game:
