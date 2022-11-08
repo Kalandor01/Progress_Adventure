43e87180be87f3b625ffff9e08e62b3f28974bec
Kalandor01 <rohovszkyakoska@gmail.com>
2022-11-08 12:39:22 +0100

    ZIP HELL

--- ziptest.py
+++ ziptest.py
@@ -2,26 +2,25 @@ import os
 import shutil
 import zipfile
 
-from tools import SAVES_FOLDER_PATH, BACKUPS_FOLDER_PATH, BACKUP_EXT, ROOT_FOLDER
+from tools import SAVES_FOLDER_PATH, BACKUPS_FOLDER_PATH, BACKUP_EXT, ROOT_FOLDER, SAVES_FOLDER, BACKUPS_FOLDER, SAVE_FOLDER_NAME_CHUNKS
+
+# REQUIRES MOVE
+def make_archive(source:str, destination:str):
+    name = os.path.basename(destination).split('.')[0]
+    archive_from = os.path.dirname(source)
+    archive_to = os.path.basename(source.strip(os.sep))
+    shutil.make_archive(name, BACKUP_EXT, archive_from, archive_to)
+    shutil.move(f"{name}.{BACKUP_EXT}", f"{destination}.{BACKUP_EXT}")
+
 
 save_name = "new save"
 zip_to = os.path.join(BACKUPS_FOLDER_PATH, save_name)
 zip_from = os.path.join(SAVES_FOLDER_PATH, save_name)
 
-# REQUIRES MOVE
-def make_archive(source, destination):
-    base = os.path.basename(destination)
-    name = base.split('.')[0]
-    archive_from = os.path.dirname(source)
-    archive_to = os.path.basename(source.strip(os.sep))
-    shutil.make_archive(name, BACKUP_EXT, archive_from, archive_to)
-    shutil.move('%s.%s'%(name,BACKUP_EXT), destination + "." + BACKUP_EXT)
-
-
-# COPES FOLDERS UP TO ROOT
+# COPIES FOLDERS UP TO ROOT
 def zipppppppp():
-    zf = zipfile.ZipFile(zip_to + ".zip", "w")
-    for dirname, subdirs, files in os.walk(zip_from):
+    zf = zipfile.ZipFile(zip_to + f".{BACKUP_EXT}", "w")
+    for dirname, subdirs, files in os.walk(os.path.join(SAVES_FOLDER, save_name)):
         zf.write(dirname)
         for filename in files:
             zf.write(os.path.join(dirname, filename))
